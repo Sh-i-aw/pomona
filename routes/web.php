94aa4976cfca5e1e7dd3dc030a8f6920b7a3b02a
Xiao Xue <xxue8761@conestogac.on.ca>
2024-09-04 16:38:42 -0400

    added HandleInertiaRequest in config needs config in vite app.blade for crucial directives, and page resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    return Inertia::render('Test'); // This will get component Test.jsx from the resources/js/Pages/Test.jsx
 });
